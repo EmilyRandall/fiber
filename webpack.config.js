cddfdac25facb768891b9bd089524fcd81a85eb7
Emily Randall <emilymrandall@comcast.net>
2016-03-31 09:52:20 -0600

    Team part 1 done

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,27 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  // execute each module with eval - fast build and reload, but not production supported
   devtool: 'eval',
   entry: {
     app : [
+      // create localhost server at localhost:3000
       'webpack-dev-server/client?http://localhost:3000',
+      // use hot module replacement, only reloading part of the code
       'webpack/hot/only-dev-server',
+      // main file is index.js
       './lib/index.js'],
   },
   output: {
+    // absolute path to output directory
     path: path.join(__dirname, './public/js/'),
+    // create main file named app.js
     filename: `app.js`,
+    // public address of output files, allows index.html to access code via path /js/app.js
     publicPath: '/js/'
   },
   plugins: [
+    // Only reload part of the code when it changes
     new webpack.HotModuleReplacementPlugin()
   ],
   node: {
@@ -22,32 +30,46 @@ module.exports = {
   },
   resolve: {
     alias: {
+      // when referencing 'react', replace with node_modules/react
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    // resolve modules using '' or '.js' extensions
     extensions: ['', '.js']
   },
   resolveLoader: {
+    // if loaders can't be found, look for them in node_modules
     'fallback': path.join(__dirname, 'node_modules')
   },
   module: {    
     loaders: [
     {
+      // for files that have a .js extension
       test: /\.js$/,
+      // use 'react-hot' and 'babel' to load code
       loaders: ['react-hot', 'babel'],
+      // don't include /node_modules
       exclude: /node_modules/,
+      // include everything in ./lib
       include: [path.join(__dirname,'./lib')]
     },
     {
+      // for files with a .xml extension
       test: /\.xml$/,
+      // use 'raw' to load
       loader: "raw"
     },
     {
+      // for files with a .json extension
       test: /\.json$/,
+      // use 'json-loader' to load code
       loaders: ['json-loader']
     },
     {
+      // for files with a .css extension
       test: /\.css?$/,
+      // use 'style' and 'raw' to load code
       loaders: ['style', 'raw'],
+      // include everthing in the current directory
       include: __dirname
     }]
   }
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,20 +3,26 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
+    // build app using ./lib/index.js
     app : [
       './lib/index.js'],
   },
   output: {
+    // absolute path to output directory
     path: path.join(__dirname, './public/js/'),
+    // create main file named app.js
     filename: `app.js`,
+    // public address of output files, allows index.html to access code via path /js/app.js
     publicPath: '/js/'
   },
   plugins: [
+    // set the process environment as a production environment
     new webpack.DefinePlugin({
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
         }
     }),
+    // Minimize all js output
     new webpack.optimize.UglifyJsPlugin({
     }),
   ],
@@ -25,32 +31,46 @@ module.exports = {
   },
   resolve: {
     alias: {
+      // when referencing 'react', replace with node_modules/react
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    // resolve modules using '' or '.js' extensions
     extensions: ['', '.js']
   },
   resolveLoader: {
+    // if loaders can't be found, look for them in node_modules
     'fallback': path.join(__dirname, 'node_modules')
   },
   module: {
     loaders: [
     {
+      // for files with a .js extension
       test: /\.js$/,
+      // use 'react-hot' and 'babel' as loaders
       loaders: ['react-hot', 'babel'],
+      // do not include node_modules
       exclude: /node_modules/,
+      // include files in ./lib
       include: [path.join(__dirname,'./lib')]
     },
     {
+      // for files with a .xml extension
       test: /\.xml$/,
+      // use 'raw' as the loader
       loader: "raw"
     },
     {
+      // for files with a .json extension
       test: /\.json$/,
+      // use 'json-loader' as the loader
       loaders: ['json-loader']
     },
     {
+      // for files with a .css extension
       test: /\.css?$/,
+      // use 'style' and 'raw' as the loaders
       loaders: ['style', 'raw'],
+      // include all files in the current directory
       include: __dirname
     }]
   }
